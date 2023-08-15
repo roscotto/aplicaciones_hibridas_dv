43251777531a7fdfa4f6b633522cbbcc64e0a2ae
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-08-15 18:16:35 -0300

    subida de archivos ej 01

---
+++ 01_uso_basico_de_node/calculos.js
@@ -0,0 +1,44 @@
+let numero1 = 10;
+let numero2 = 20;
+
+let arrayNumeros = [10, 50, 35, 2, 20, 33, 88, 72];
+
+//4 Crear una función que reciba dos números y retorne la suma de ellos
+
+function suma() {
+    return numero1 + numero2;
+}
+
+console.log('Suma:', suma());
+
+//5 Crear una función que reciba dos números y devuelva el resultado entre la división del primero sobre el segundo
+
+
+function division() {
+    return numero1 / numero2;
+}
+
+console.log('División:',division());
+
+
+// 6 Crear una función que reciba un Array de N elementos y devuelva el mayor valor de ese array
+
+function encontrarMayor() {
+    let numeroMayor = Math.max(...arrayNumeros);
+    return numeroMayor;
+}
+
+console.log('El número mayor es:', encontrarMayor());
+
+// 7. Hacer uso del console.log para mostrar la suma de 5 y 10
+console.log('Suma de 5 y 10:', 5 + 10);
+
+
+//8. Hacer uso del console.log para mostrar la división de 20 y 2 (que sucede si envió 20 y 0)
+
+console.log('Division de 20 y 2:', 20 / 2);
+console.log('Division de 20 y 0:', 20 / 0);
+
+// 9. Hacer uso del console.log para mostrar el mayor de la lista [2,8,9,7,5,6]
+
+console.log('Mayor de la lista:', Math.max(...[2,8,9,7,5,6]));
